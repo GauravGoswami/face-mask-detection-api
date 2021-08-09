8e99bd0d747dd6171042d7040dc9d23c5c94c6aa
Ramesh <ramesh.poomalai@in.ibm.com>
2021-08-10 05:27:52 +0530

    Updated

--- deployscripts/startbuild.sh
+++ deployscripts/startbuild.sh
@@ -1,2 +1,3 @@
-
-tkn pipeline start fscloud-asset-pipeline -w name=shared-workspace,claimName=source-pvc -p deployment-name=mask-detection -p  git-url=https://github.ibm.com/IBM-India-CTO-ENG-Assets/FundtransferService.git  -p IMAGE=image-registry.openshift-image-registry.svc:5000/ml-assets/mask-detection -p git-revision=master  -p helm-git-revision=main  -p sourcerepo=src -p targetrepo=helmrepo -p git-helm-url=https://github.com/rameshpoomalai/mcmrepo.git
+oc create -f pipeline.yaml
+oc create -f striggerevents.yaml
+tkn pipeline start fscloud-asset-pipeline -w name=shared-workspace,claimName=source-pvc -p deployment-name=mask-detection -p  git-url=https://github.com/GauravGoswami/face-mask-detection-api.git  -p IMAGE=image-registry.openshift-image-registry.svc:5000/ml-assets/mask-detection -p git-revision=master  -p helm-git-revision=main  -p sourcerepo=src -p targetrepo=helmrepo -p git-helm-url=https://github.com/rameshpoomalai/mcmrepo.git
